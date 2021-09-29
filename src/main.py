85441d06a70aaaa14efdcc9833b3c5a594d427f4
Thomasvtn <vaton-thomas@outlook.fr>
2021-09-29 19:44:28 +0200

    update gitignore and launch bot

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import discord
 from discord.ext import commands
 
 bot = commands.Bot(
@@ -6,7 +7,7 @@ bot = commands.Bot(
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 0000000  # Change to your discord id!!!
+bot.author_id = 234708273851400192 # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -17,5 +18,15 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_SUPER_TOKEN>"
+token = "ODkyODIyOTE0NTQwMzk2NjQ0.YVSgIg.ZkDX0x4aCYuf3uAda5NReJBW0KI"
 bot.run(token)  # Starts the bot
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')

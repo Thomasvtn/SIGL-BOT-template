21ef5b0e49065118672dc13f142e9ddd8488a1ad
Thomasvtn <vaton-thomas@outlook.fr>
2021-09-29 19:53:05 +0200

    Add !name command

---
+++ src/commandsFile.py
@@ -0,0 +1,4 @@
+from discord.ext import commands
+
+def name(ctx):
+        return ctx.author
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import discord
 from discord.ext import commands
 
+import commandsFile
+
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True  # Commands aren't case-sensitive
@@ -15,8 +17,13 @@ async def on_ready():  # When the bot is ready
     print(bot.user)  # Prints the bot's username and identifier
 
 @bot.command()
-async def pong(ctx):
+async def ping(ctx):
     await ctx.send('pong')
+
+
+@bot.command()
+async def name(ctx):
+    await ctx.send(commandsFile.name(ctx))
 
 token = "ODkyODIyOTE0NTQwMzk2NjQ0.YVSgIg.ZkDX0x4aCYuf3uAda5NReJBW0KI"
 bot.run(token)  # Starts the bot
